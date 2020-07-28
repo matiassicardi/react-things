96dfc4f7cfa28467e73c9fb871aab986847007b6
Matias Sicardi <57410993+matiassicardi@users.noreply.github.com>
2020-07-28 08:48:44 -0300

    Seting Up App.js

---
+++ insurance-quote/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './components/Header'
+
+
+function App() {
+  return (
+    <Header 
+      titulo='Cotizador de Seguros'
+    />
+  );
+}
+
+export default App;
