68f59b801d8cc0f45ad464679c720f7e60fe5a21
Matias Sicardi <57410993+matiassicardi@users.noreply.github.com>
2020-07-29 09:11:12 -0300

    Adding Styled Component to Form

--- insurance-quote/src/App.js
+++ insurance-quote/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-import Header from './components/Header'
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+
+import styled from '@emotion/styled';
+
+const Contendedor = styled.div `
+  max-width: 600px;
+  margin: 0 auto;
+`;
+const ContenedorFormulario = styled.div `
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 
 function App() {
   return (
-    <Header 
-      titulo='Cotizador de Seguros'
-    />
+    <Contendedor>
+      <Header 
+        titulo='Cotizador de Seguros'
+      />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contendedor>
   );
 }
 
